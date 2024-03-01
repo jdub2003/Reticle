1b4c1cd63146443954a501b993e18101ee8d5524
Jordan DuBose <dubosejordan13@gmail.com>
2024-03-01 11:48:25 -0500

    AudioSource added but not fully functioning yet

--- Assets/ClarinetScript.cs
+++ Assets/ClarinetScript.cs
@@ -10,49 +10,50 @@ public class ClarinetScript : MonoBehaviour
     [SerializeField] AudioClip pat;
     [SerializeField] float speed=4f;
     // Start is called before the first frame update
-    private AudioSource audioSource;
+    public AudioSource audioSource;
     void Start()
     {
-        AudioSource=GetComponent<AudioSource>();
+        //audioSource=GetComponent<AudioSource>();
         Destroy(gameObject, 3.6f);
     }
-
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Time.deltaTime * speed * Vector3.right);
-
-        AudioSource souce = GetComponent<AudioSource>();
-        souce.Play();
     }
     //To implement point system
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        audioSource = GetComponent<AudioSource>();
         GameManager gm;
         gm = FindAnyObjectByType<GameManager>();
 
         if (collision.CompareTag("Patrick"))
         {
-            AudioSource.
-            collision.gameObject.GetComponent<AudioSource>().Play();
+            audioSource.PlayOneShot(pat);
             gm.Score = gm.Score + 10;
+            Destroy(collision.gameObject);//destroy object we hit
+            Destroy(gameObject);
 
         }
         if (collision.CompareTag("Spongebob"))
         {
-            collision.gameObject.GetComponent<AudioSource>().Play();
+            audioSource.PlayOneShot(sb);
             gm.Score = gm.Score + 10;
+            Destroy(collision.gameObject);//destroy object we hit
+            Destroy(gameObject);
 
         }
         if (collision.CompareTag("Handsome"))
         {
-            collision.gameObject.GetComponent<AudioSource>().Play();
+            audioSource.PlayOneShot(clarinet);
             gm.Score = gm.Score - 15;
+            Destroy(collision.gameObject);//destroy object we hit
+            Destroy(gameObject);
         }
-
         //Debug.Log("Im triggered");
-        Destroy(collision.gameObject);//destroy object we hit
-        Destroy(gameObject);//destroy the fireball itself
+        //Destroy(collision.gameObject);//destroy object we hit
+        //Destroy(gameObject);//destroy the fireball itself
        // find and return first object with gamemanager component
     }
 }
