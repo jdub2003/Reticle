6c00ad4ec50597b03a4c1be7511a497766d10218
Jordan DuBose <dubosejordan13@gmail.com>
2024-03-01 14:01:37 -0500

    Fixed Prefab Deletions and Collideres

--- Assets/ClarinetScript.cs
+++ Assets/ClarinetScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 
 public class ClarinetScript : MonoBehaviour
@@ -8,52 +9,60 @@ public class ClarinetScript : MonoBehaviour
     [SerializeField] AudioClip sb;
     [SerializeField] AudioClip clarinet;
     [SerializeField] AudioClip pat;
-    [SerializeField] float speed=4f;
+    [SerializeField] float speed;
+  
+    private AudioSource audioSource;
+
     // Start is called before the first frame update
-    public AudioSource audioSource;
     void Start()
     {
         //audioSource=GetComponent<AudioSource>();
         Destroy(gameObject, 3.6f);
     }
+
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Time.deltaTime * speed * Vector3.right);
+        transform.Translate(Time.deltaTime * 4f * Vector3.right);
     }
+
+
     //To implement point system
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        audioSource = GetComponent<AudioSource>();
+        //audioSource = GetComponent<AudioSource>();
         GameManager gm;
         gm = FindAnyObjectByType<GameManager>();
 
-        if (collision.CompareTag("Patrick"))
+        if (collision.CompareTag("Patrick")==true)
         {
+            audioSource=collision.gameObject.GetComponent<AudioSource>();
             audioSource.PlayOneShot(pat);
             gm.Score = gm.Score + 10;
-            Destroy(collision.gameObject);//destroy object we hit
-            Destroy(gameObject);
+            //Destroy(collision.gameObject);//destroy object we hit
+            //Destroy(gameObject);
 
         }
-        if (collision.CompareTag("Spongebob"))
+        if (collision.CompareTag("Spongebob")==true)
         {
+            audioSource = collision.gameObject.GetComponent<AudioSource>();
             audioSource.PlayOneShot(sb);
             gm.Score = gm.Score + 10;
-            Destroy(collision.gameObject);//destroy object we hit
-            Destroy(gameObject);
+            //Destroy(collision.gameObject);//destroy object we hit
+            //Destroy(gameObject);
 
         }
-        if (collision.CompareTag("Handsome"))
+        if (collision.CompareTag("Handsome")==true)
         {
+            audioSource = collision.gameObject.GetComponent<AudioSource>();
             audioSource.PlayOneShot(clarinet);
             gm.Score = gm.Score - 15;
-            Destroy(collision.gameObject);//destroy object we hit
-            Destroy(gameObject);
+            //Destroy(collision.gameObject);//destroy object we hit
+            //Destroy(gameObject);
         }
         //Debug.Log("Im triggered");
-        //Destroy(collision.gameObject);//destroy object we hit
-        //Destroy(gameObject);//destroy the fireball itself
+        Destroy(collision.gameObject);//destroy object we hit
+        Destroy(gameObject);//destroy the fireball itself
        // find and return first object with gamemanager component
     }
 }
